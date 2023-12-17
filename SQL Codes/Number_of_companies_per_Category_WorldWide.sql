cee8dc93b4ea1c16a8942c97ddce33edb3d416c8
saamehg <golzadeh.s@gmail.com>
2023-12-17 23:05:53 +0100

    SQL code of firt plot in presentation

---
+++ SQL Codes/Number_of_companies_per_Category_WorldWide.sql
@@ -0,0 +1,13 @@
+SELECT
+  country_code,
+  status,
+  COUNT(DISTINCT name) AS number_companies_per_country
+FROM
+  tutorial.crunchbase_companies
+WHERE
+  country_code IS NOT NULL --and status = 'operating'
+GROUP BY
+  country_code,
+  status
+ORDER BY
+  number_companies_per_country DESC
